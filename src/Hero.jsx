fd963f6df03dd4b7a9c2d2553890213d8e93ff9a
sashashreyder <sashashreiderbr@gmail.com>
2024-11-21 14:50:01 -0300

    hello

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,6 +5,7 @@ function Hero() {
     <section className="hero">
       <h1>Bringing Your Ideas to Life</h1>
       <p className="tagline">Design. Capture. Create.</p>
+      <h2>Hello</h2>
     </section>
   );
 }
